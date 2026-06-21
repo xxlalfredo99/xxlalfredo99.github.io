1b9a2e660905db721382e4f411b13ebd65deaf8d
Alfred Tze-Hong Ha <alfredhongha@gmail.com>
2026-06-21 10:52:16 -0400

    Initial commit

---
+++ about-me/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import About from './components/About';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <About />
+    </div>
+  );
+}
+
+export default App;
