1a0b7290f04cd1995902dbd0ff86ece9a17049bc
Alfred Tze-Hong Ha <alfredhongha@gmail.com>
2026-06-25 04:04:34 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/xxlalfredo99.github.io/", // replace with your repo name
+  base: "/", // replace with your repo name
 })
